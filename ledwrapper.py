8ccd9ddb9e5908c4493e824431d2c7a5c4ed84c1
CoolPro <coolpro@gmx.de>
2021-01-17 13:23:55 +0100

    zahlen

--- ledwrapper.py
+++ ledwrapper.py
@@ -48,10 +48,11 @@ class Wrapper:
     def zahl_speichern(self,zahl,reihe,helligkeit):
         self.buffer=[]
         buffer=self.zahl_zu_bit(zahl)
-        for i in range ((reihe*8),((reihe*8)+31)):
-            if self.buffer[i]==1:
+        self.merke=reihe*8
+        for i in range (merke,(merke+31)):
+            if self.buffer[i-merke]==1:
                 self.ledArray[i][2]=helligkeit
-            elif self.buffer[i]==0:
+            elif self.buffer[i-merke]==0:
                 self.ledArray[i][2]=0   
                 
     
