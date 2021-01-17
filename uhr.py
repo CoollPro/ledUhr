3cff5c1c67be169b2ab4b9c20057b23cd7545c51
CoolPro <coolpro@gmx.de>
2021-01-17 14:29:26 +0100

    zahlen

--- uhr.py
+++ uhr.py
@@ -19,12 +19,12 @@ strip.begin()
 wrapper=ledwrapper.Wrapper(strip)
 wrapper.setHelligkeit(0.0)
 wrapper.setAllSaturation(1)
-wrapper.setAllColour(0.16)
-wrapper.zahl_speichern(5,0,0.1) #zahlen müssen im geraden Bereich sein
-wrapper.zahl_speichern(6,6,0.1)
-wrapper.zahl_speichern(7,12,0.1)
-wrapper.zahl_speichern(8,18,0.1)
-wrapper.zahl_speichern(9,24,0.1)
+wrapper.setAllColour(0.84)
+wrapper.zahl_speichern(5,0,0.5) #zahlen müssen im geraden Bereich sein
+wrapper.zahl_speichern(6,6,0.5)
+wrapper.zahl_speichern(7,12,0.5)
+wrapper.zahl_speichern(8,18,0.5)
+wrapper.zahl_speichern(9,24,0.5)
 
 
 wrapper.ausgeben()
