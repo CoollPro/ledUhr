1466558675273ffdd00b610eca70991aa45bce1a
CoolPro <coolpro@gmx.de>
2021-01-17 13:49:41 +0100

    zahlen

--- ledwrapper.py
+++ ledwrapper.py
@@ -50,10 +50,10 @@ class Wrapper:
         self.buffer=[]
         buffer=self.zahl_zu_bit(zahl)
         self.merke=(reihe*8)
-        print(buffer[0])
+        print(buffer)
 
         for i in range (self.merke,(self.merke+31)):
-            print(buffer)
+            print(buffer[0])
             self.bufferIndex=i-self.merke
 
             if self.buffer[self.bufferIndex]==1:
