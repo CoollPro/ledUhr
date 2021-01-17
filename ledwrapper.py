9a91c8a5e35e80733b0907483bd77014ef0fd64e
CoolPro <coolpro@gmx.de>
2021-01-17 13:36:03 +0100

    zahlen

--- ledwrapper.py
+++ ledwrapper.py
@@ -50,10 +50,12 @@ class Wrapper:
         buffer=self.zahl_zu_bit(zahl)
         self.merke=(reihe*8)
 
+        print(buffer)
+
         for i in range (self.merke,(self.merke+31)):
-            if self.buffer[i-self.merke]==1:
+            if self.buffer[(i-self.merke)]==1:
                 self.ledArray[i][2]=helligkeit
-            elif self.buffer[i-self.merke]==0:
+            elif self.buffer[(i-self.merke)]==0:
                 self.ledArray[i][2]=0   
                 
     
