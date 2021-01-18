ae93c9909aa9cc6f90eeb7856450d37ead23516e
CoolPro <coolpro@gmx.de>
2021-01-18 15:44:14 +0100

    Uhrzeit

--- uhr.py
+++ uhr.py
@@ -22,7 +22,9 @@ wrapper=ledwrapper.Wrapper(strip)
 wrapper.setHelligkeit(0.0)
 wrapper.setAllSaturation(1)
 wrapper.setAllColour(0.84)
-wrapper.doppelte_zahl_speichern(10,0,0.2)
+wrapper.doppelte_zahl_speichern(9,0,0.2)
+wrapper.doppelte_zahl_speichern(60,12,0.2)
+wrapper.doppelte_zahl_speichern(35,24,0.2)
 
 
 wrapper.ausgeben()
