b873465df509bdfc2b57206de03d066432c7565f
CoolPro <coolpro@gmx.de>
2021-01-18 16:09:24 +0100

    Uhrzeit

--- uhr.py
+++ uhr.py
@@ -25,10 +25,6 @@ wrapper.setAllColour(0.84)
 
 while 1:
     for i in range (100):
-    wrapper.doppelte_zahl_speichern(i,1,0.2)
-    wrapper.ausgeben()
-    time.sleep(0,5)
-
-
-
-    
+        wrapper.doppelte_zahl_speichern(i,1,0.2)
+        wrapper.ausgeben()
+        time.sleep(0,5)
